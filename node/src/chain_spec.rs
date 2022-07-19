49a6b772e44b890ebeca4aea0eeaff480e5aed6e
Alec Ghazarian <me@alec.pro>
2022-07-19 17:24:35 -0400

    allow debug output

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -55,6 +55,8 @@ pub fn development_config() -> Result<ChainSpec, String> {
 				// Pre-funded accounts
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
 					get_account_id_from_seed::<sr25519::Public>("tribal"),
 					get_account_id_from_seed::<sr25519::Public>("smartalec"),
 				],
@@ -94,6 +96,8 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 				// Pre-funded accounts
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
 					get_account_id_from_seed::<sr25519::Public>("tribal"),
 					get_account_id_from_seed::<sr25519::Public>("smartalec"),
 				],
